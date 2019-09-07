d5a466c10e20afe0f1aad0d1b59d6a393454d4fc
David Strauch <dmaster18@gmail.com>
2019-09-07 15:25:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,8 +7,8 @@ class Author
   end
     
   def add_post(post_object)
-    @posts << post_object
     post_object.author = self
+        @posts << post_object
   end
   
   def add_post_by_title(title)
